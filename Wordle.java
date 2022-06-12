5c32a648770f05b043fed3b68cad120fc9fc4b34
noahdj37 <noahdj37@gmail.com>
2022-06-11 21:36:18 -0700

    Import Local Files

---
+++ Wordle.java
@@ -0,0 +1,21 @@
+package Wordle;
+import java.util.Scanner;
+import java.util.random.*;
+
+class Wordle{
+
+    public static String genWord(){
+        String toReturn = "";
+
+
+        return toReturn;
+    }
+
+
+
+
+    public static void main(String[] args){
+
+    }
+}
+
---
+++ WordleAbstract.java
@@ -0,0 +1,5 @@
+package Wordle;
+
+public class WordleAbstract {
+    
+}
