b6ec8115acaf1ecee175195e3e1902c14f2d43bd
njaurigue <noahdj37@gmail.com>
2022-06-12 11:07:42 -0700

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -8,14 +8,12 @@ public class Wordle extends WordleAbstract{
     public static String genWord(){
         String toReturn = "";
         Random r = new Random(SEED);
+
         int n = r.nextInt(words.size());
         String word = words.get(n);
         System.out.println(word);
         return toReturn;
     }
-
-
-
 
     public static void main(String[] args){
         genWord();
