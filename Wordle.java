e7c48141cf228ac13279a24c8800682d17cd092c
njaurigue <noahdj37@gmail.com>
2022-06-14 10:57:24 -0700

    Further work

--- Wordle.java
+++ Wordle.java
@@ -1,22 +1,24 @@
-package Wordle;
 import java.util.Scanner;
+import java.io.IOException;
 import java.util.Random;
 
 
 public class Wordle extends WordleAbstract{
 
-    public static String genWord(){
-        String toReturn = "";
-        Random r = new Random(SEED);
+    public static String genWord(String s) throws IOException{
+        Random r = new Random();
 
-        int n = r.nextInt(words.size());
-        String word = words.get(n);
-        System.out.println(word);
-        return toReturn;
+        int n = r.nextInt(getWord(s).length);
+        String word = getWord(s)[n];
+        return word;
     }
 
-    public static void main(String[] args){
-        genWord();
+    public static void main(String[] args) throws IOException{
+        String answer = genWord(args[0]);
+        Scanner s = new Scanner(System.in);
+
     }
+
+    
 }
 
--- WordleAbstract.java
+++ WordleAbstract.java
@@ -1,16 +1,21 @@
-package Wordle;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public abstract class WordleAbstract {
 
-    public static String[] getWords(String path) throws IOException{
+    public static final int EXIT_WIN = 0;
+    public static final int EXIT_LOSE = -1;
+
+    public static String[] getWord(String path) throws IOException{
         String textFile = Files.readString(Paths.get(path));
         String[] words = textFile.split(",");
         return words;
     }
 
-    public final static int SEED = 99;
+    public static int printField(String answer, String move, int guess){
+          
+        return EXIT_WIN;
+    }
 }
