f7baea76bef7121f6d7c6b4b1d3c89ddde0d63af
njaurigue <noahdj37@gmail.com>
2022-07-03 21:15:22 -0700

    Update app.js
    add losing scenario

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ document.addEventListener("keydown", e => {
         if(e.key == "Enter"){
             if(compareWords(solution, curr, row)){
                 gameWin();
+            }else if(row == 6){
+                console.warn("You Lose, the word was: " + solution);
             }
             row++;
             char = 0;
